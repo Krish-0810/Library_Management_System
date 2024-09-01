42d0b17a075907846a208bc98cfb72627aeca083
krish <krishpatel85195@gmail.com>
2024-09-01 17:26:47 +0530

    Add test for Return book

--- Lab1JAVA/src/main/java/org/example/LibraryTest.java
+++ Lab1JAVA/src/main/java/org/example/LibraryTest.java
@@ -22,5 +22,14 @@ class LibraryTest {
 
     @Test
     void testBorrowBookUnavailable() {
+        Book book = new Book("978-3-16-148410-0", "Clean Code", "Robert C. Martin", 2008);
+        library.addBook(book);
+        assertThrows(Exception.class, () -> {
+            library.borrowBook("978-3-16-148410-0");
+            library.borrowBook("978-3-16-148410-0");
+        });
+    }
+    @Test
+    void testReturnBook() throws Exception {
         
     }
